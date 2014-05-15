5e3092ca60c9458e3820641b36e1497543c3968e
stujones11 <stujones111@gmail.com>
2014-05-15 21:14:00 +0100

    Add config options

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Makes hand wielded items visible to other players.
 
 Please note this mod is currently not compatible with minetest-3d_armor or any other mod that changes the default character model.
 
+See wield3d.conf.example for config options (save as wield3d.conf)
+
--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/location.lua")
+WIELD3D_INIT_DELAY = 1
+WIELD3D_RETRY_TIME = 10
+WIELD3D_UPDATE_TIME = 1
 
-local update_time = 2 -- number of seconds between wielditem updates
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+local input = io.open(modpath.."/wield3d.conf", "r")
+if input then
+	dofile(modpath.."/wield3d.conf")
+	input:close()
+	input = nil
+end
+dofile(modpath.."/location.lua")
+
 local location = {
 	"Arm_Right",           -- default bone
 	{x=0.2, y=5.5, z=3},   -- default position
@@ -14,8 +24,10 @@ local function add_wield_entity(player)
 	local pos = player:getpos()
 	local inv = player:get_inventory()
 	if name and pos and inv then
-		local object = minetest.add_entity(pos, "wield3d:wield_entity")
+		local offset = {x=pos.x, y=pos.y + 0.5, z=pos.z}
+		local object = minetest.add_entity(offset, "wield3d:wield_entity")
 		if object then
+			object:set_properties({collisionbox={0,0,0, 0,0,0}})
 			object:set_attach(player, location[1], location[2], location[3])
 			local entity = object:get_luaentity()
 			if entity then
@@ -35,7 +47,7 @@ minetest.register_item("wield3d:hand", {
 
 minetest.register_entity("wield3d:wield_entity", {
 	physical = false,
-	collisionbox = {x=0, y=0, z=0},
+	collisionbox = {-0.125,-0.125,-0.125, 0.125,0.125,0.125},
 	visual = "wielditem",
 	visual_size = {x=0.25, y=0.25},
 	textures = {"wield3d:hand"},
@@ -45,35 +57,40 @@ minetest.register_entity("wield3d:wield_entity", {
 	location = {location[1], location[2], location[3]},
 	on_step = function(self, dtime)
 		self.timer = self.timer + dtime
-		if self.timer < update_time then
+		if self.timer < WIELD3D_UPDATE_TIME then
 			return
 		end
 		self.timer = 0
 		local player = self.player
 		if player then
-			local pos = player:getpos()
-			if pos then
-				local stack = player:get_wielded_item()
-				local item = stack:get_name()
-				if item == self.item then
+			local name = player:get_player_name()
+			local p1 = player:getpos()
+			local p2 = self.object:getpos()
+			if p1 and p2 then
+				if vector.equals(p1, p2) then
+					local stack = player:get_wielded_item()
+					local item = stack:get_name()
+					if item == self.item then
+						return
+					end
+					self.item = item
+					if item == "" then
+						item = "wield3d:hand"
+					end
+					local loc = wield3d_location[item] or location
+					if loc[1] ~= self.location[1]
+					or vector.equals(loc[2], self.location[2]) == false
+					or vector.equals(loc[3], self.location[3]) == false then
+						self.object:setpos(p2)
+						self.object:set_detach()
+						self.object:set_attach(player, loc[1], loc[2], loc[3])
+						self.location = {loc[1], loc[2], loc[3]}
+					end
+					self.object:set_properties({textures={item}})
 					return
 				end
-				self.item = item
-				if item == "" then
-					item = "wield3d:hand"
-				end
-				local loc = wield3d_location[item] or location
-				if loc[1] ~= self.location[1]
-				or vector.equals(loc[2], self.location[2]) == false
-				or vector.equals(loc[3], self.location[3]) == false then
-					self.object:setpos(pos)
-					self.object:set_detach()
-					self.object:set_attach(player, loc[1], loc[2], loc[3])
-					self.location = {loc[1], loc[2], loc[3]}
-				end
-				self.object:set_properties({textures={item}})
-				return
 			end
+			player_wielding[name] = 0
 		end
 		self.object:remove()
 	end,
@@ -81,7 +98,7 @@ minetest.register_entity("wield3d:wield_entity", {
 
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
-	if timer > 10 then
+	if timer > WIELD3D_RETRY_TIME then
 		for name, state in pairs(player_wielding) do
 			if state == 0 then
 				local player = minetest.get_player_by_name(name)
@@ -106,7 +123,7 @@ end)
 minetest.register_on_joinplayer(function(player)
 	default.player_set_model(player, "wield3d_character.b3d")
 	player_wielding[player:get_player_name()] = 0
-	minetest.after(1, add_wield_entity, player)
+	minetest.after(WIELD3D_INIT_DELAY, add_wield_entity, player)
 end)
 
 default.player_register_model("wield3d_character.b3d", {
